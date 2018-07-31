8a9f817ff193506062402f550f4b63de31fa47e0
cagatayyildiz87 <cagatayyildiz87@gmail.com>
2018-07-31 12:21:25 +0300

    Add project files.

---
+++ CoreSecurity/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CoreSecurity.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreSecurity.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (LoginUser(loginModel.Username, loginModel.Password))
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, loginModel.Username)
+            };
+
+                var userIdentity = new ClaimsIdentity(claims, "login");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+
+                //Just redirect to our index after logging in. 
+                return RedirectToAction("Index","Home");
+            }
+            return View();
+        }
+
+        private bool LoginUser(string username, string password)
+        {
+            //As an example. This method would go to our data store and validate that the combination is correct. 
+            //For now just return true. 
+            return true;
+        }
+    }
+}
